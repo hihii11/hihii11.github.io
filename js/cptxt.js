1914341a0fb1173dda549cdc5042cb9858efc7e1
hihii11 <937938366@qq.com>
2022-01-26 10:43:20 +0800

    Blog

--- js/cptxt.js
+++ js/cptxt.js
@@ -1,15 +1,15 @@
 function copyText(element,area) {
 		var fstline = area.firstLine();
 		var lstline = area.lastLine();
-		var text ="";
+		var value ="";
 		var i = fstline;
 		while(i<=lstline)
 		{
-			text = text + area.getLine(i)+"\r\n";
+			value = value + area.getLine(i)+"\r\n";
 			i=i+1;
 		}
-		document.getElementById("copy_textarea").innerText = text;
-
+		document.getElementById("copy_textarea").innerText = value;
+		
 		 var text = document.getElementById(element);
 		if (document.body.createTextRange) {
 			var range = document.body.createTextRange();
@@ -24,11 +24,13 @@ function copyText(element,area) {
 		} else {
 			console.log('none');
 		}
-
 }
 
-function cp_code(area)
-{
-	if(document.readyState == "complete")
+	function cp_code(area)
+	{
+		
+		document.getElementById("copy_textarea").style.display="block";
 		copyText("copy_textarea",area);document.execCommand("Copy");
-}
+		document.getElementById("copy_textarea").style.display="none";
+	}
+    
