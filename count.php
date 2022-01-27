da5eabe03306da02b9c05e5116255e4770d2d670
hihii11 <937938366@qq.com>
2022-01-27 19:49:13 +0800

    blog

---
+++ count.php
@@ -0,0 +1,1 @@
+ <?php mysql_connect('localhost','root',''); mysql_selectdb('click'); $rs=mysql_query("UPDATE click SET num = num +1 WHERE name = '".$_GET['a']."'"); if(mysql_affected_rows()==1){ $rs=mysql_query("select * from click where name='".$_GET['a']."'"); $row=mysql_fetch_array($rs); echo $row['num']; }?>
